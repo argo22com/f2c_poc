2843e24f2e0760e1f8bbaa2497586067d52127ad
Martin Peka <martin.peka@argo22.com>
2022-10-19 17:42:31 +0200

    TRS001-210: Current WIP

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
 #!/bin/python
 
 import fields2cover as f2c
+from osgeo import ogr
+
+def robot():
+    r = f2c.Robot(2.357, 2.357)
+    r.cruise_speed = 0.5
+    r.linear_curv_change = 1.0
+    #r.max_vel = 1.0
+    r.setMinRadius(3.0)
+    return r
+
+def point(lat, lon):
+    ogrpoint = ogr.Geometry(ogr.wkbPoint)
+    ogrpoint.AddPoint(float(lat), float(lon))
+    point = f2c.Point()
+    point.importFromWkt(ogrpoint.ExportToWkt())
+    return point
 
 boundaries = [
         [48.941325, 14.452474],
@@ -14,12 +30,22 @@ boundaries = [
 
 ring = f2c.LinearRing()
 for i in boundaries:
-    point = f2c.Point()
-    point.importFromWkt("POINT (" + str(i[0]) + " " + str(i[1]) + " 0)")
-    ring.addPoint(point)
+    ring.addPoint(point(str(i[0]), str(i[1])))
+# add the first point again to close the circle
+ring.addPoint(point(str(boundaries[0][0]), str(boundaries[0][1])))
 
-print("Area of the ring space: ", ring.getArea())
+cell = f2c.Cell(ring)
+cells = f2c.Cells(cell)
+field = f2c.Field(cells, "Test-field")
 
-f2c.Visualizer.plot(ring)
+robot = robot()
+const_hl = f2c.HG_Const_gen();
+no_hl = const_hl.generateHeadlands(cells, 3.0 * robot.robot_width)
+
+print("Area of the field space: ", field.getArea())
+print("Area of the field w/o headlands: ", no_hl.getArea())
+f2c.Visualizer.figure(100)
+f2c.Visualizer.plot(field)
+f2c.Visualizer.plot(no_hl)
 f2c.Visualizer.show()
-f2c.Visualizer.save("/output/xxx-image")
+f2c.Visualizer.save("/output/field_with_headland")
